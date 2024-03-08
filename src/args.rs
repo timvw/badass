7b9f1cc95817b2e4e2742ab9aba97ca7ef1adf61
Tim Van Wassenhove <tim@timvw.be>
2024-03-08 13:41:54 +0100

    style: movethings around

---
+++ src/args.rs
@@ -0,0 +1,30 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "badass", version)]
+pub struct BadassArgs {
+    #[clap(flatten)]
+    pub global_opts: GlobalOpts,
+
+    #[clap(subcommand)]
+    pub command: Command,
+}
+
+impl BadassArgs {
+    pub fn parse() -> Self {
+        Parser::parse()
+    }
+}
+
+// https://rust-cli-recommendations.sunshowers.io/handling-arguments.html
+
+#[derive(Debug, Args)]
+pub struct GlobalOpts {}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Render the sql templates
+    Compile,
+    /// Materialize the templates
+    Materialize,
+}
