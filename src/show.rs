fa179084a0fdaf53b57ec41237d7b40ce6574ffd
Tim Van Wassenhove <tim@timvw.be>
2024-03-08 22:22:36 +0100

    further progress in executing a query

--- src/show.rs
+++ src/show.rs
@@ -1,7 +1,26 @@
 use crate::args::{BadassArgs, ShowArgs};
 use crate::settings::Settings;
+use postgres::{Client, NoTls};
 
 pub fn do_show(settings: &Settings, show_args: &ShowArgs) -> anyhow::Result<()> {
     log::info!("Need to show {show_args:?}");
+
+    // find the model sql...
+    let compiled_model_sql = r"
+    SELECT 1 AS number
+    UNION ALL
+    SELECT 3 AS number
+    ";
+
+    let mut client = Client::connect("host=localhost user=tim", NoTls)?;
+
+    for row in client.query(compiled_model_sql, &[])? {
+        for column in row.columns() {
+            println!("column: {}", column.name());
+        }
+        let number: i32 = row.get(0);
+        println!("found row: {}", number,);
+    }
+
     Ok(())
 }
